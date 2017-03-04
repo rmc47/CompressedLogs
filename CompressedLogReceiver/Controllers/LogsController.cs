af663ff8f001585282fd744229a9c5214f037dff
Robert Chipperfield <robert@syxis.co.uk>
2017-03-04 15:08:30 -0500

    Return error message if there is one

--- CompressedLogReceiver/Controllers/LogsController.cs
+++ CompressedLogReceiver/Controllers/LogsController.cs
@@ -18,39 +18,46 @@ namespace CompressedLogReceiver.Controllers
 
         public ActionResult Submit(int qsoCount, string hash)
         {
-            if (qsoCount < 1)
-                return new HttpStatusCodeResult(400, "Invalid QSO count");
-            if (string.IsNullOrWhiteSpace(hash))
-                return new HttpStatusCodeResult(400, "Missing hash");
-
-            int length = Request.ContentLength;
-            byte[] incomingData = new byte[length];
-            using (Stream requestStream = Request.InputStream)
+            try
             {
-                int pos = 0;
-                while (pos < length)
+                if (qsoCount < 1)
+                    return new HttpStatusCodeResult(400, "Invalid QSO count");
+                if (string.IsNullOrWhiteSpace(hash))
+                    return new HttpStatusCodeResult(400, "Missing hash");
+
+                int length = Request.ContentLength;
+                byte[] incomingData = new byte[length];
+                using (Stream requestStream = Request.InputStream)
                 {
-                    pos += requestStream.Read(incomingData, pos, length - pos);
+                    int pos = 0;
+                    while (pos < length)
+                    {
+                        pos += requestStream.Read(incomingData, pos, length - pos);
+                    }
+                }
+
+                List<Qso> submittedQsos = new List<Qso>(qsoCount);
+                QsoCompressor compressor = new QsoCompressor();
+                int decompressPos = 0;
+                for (int i = 0; i < qsoCount; i++)
+                {
+                    submittedQsos.Add(compressor.UncompressQso(incomingData, ref decompressPos));
+                }
+                if (decompressPos != length)
+                    return new HttpStatusCodeResult(400, "Length does not match expected length");
+
+                QsoStore store = new QsoStore(ConfigurationSettings.AppSettings["DatabasePath"]);
+                foreach (Qso q in submittedQsos)
+                {
+                    if (!store.QsoExists(q))
+                    {
+                        store.AddQso(q);
+                    }
                 }
             }
-
-            List<Qso> submittedQsos = new List<Qso>(qsoCount);
-            QsoCompressor compressor = new QsoCompressor ();
-            int decompressPos = 0;
-            for (int i = 0; i < qsoCount; i++)
+            catch (Exception ex)
             {
-                submittedQsos.Add(compressor.UncompressQso(incomingData, ref decompressPos));
-            }
-            if (decompressPos != length)
-                return new HttpStatusCodeResult(400, "Length does not match expected length");
-
-            QsoStore store = new QsoStore(ConfigurationSettings.AppSettings["DatabasePath"]);
-            foreach (Qso q in submittedQsos)
-            {
-                if (!store.QsoExists(q))
-                {
-                    store.AddQso(q);
-                }
+                return Content(ex.ToString());
             }
             return Content("OK");
         }
